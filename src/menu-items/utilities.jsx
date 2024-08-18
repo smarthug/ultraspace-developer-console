130cae38cdc73ff442c19498295ee53d9d050ba6
smarthug <kirklayer@gmail.com>
2024-08-18 18:22:30 +0900

    init

---
+++ src/menu-items/utilities.jsx
@@ -0,0 +1,66 @@
+// assets
+import {
+  AppstoreAddOutlined,
+  AntDesignOutlined,
+  BarcodeOutlined,
+  BgColorsOutlined,
+  FontSizeOutlined,
+  LoadingOutlined
+} from '@ant-design/icons';
+
+// icons
+const icons = {
+  FontSizeOutlined,
+  BgColorsOutlined,
+  BarcodeOutlined,
+  AntDesignOutlined,
+  LoadingOutlined,
+  AppstoreAddOutlined
+};
+
+// ==============================|| MENU ITEMS - UTILITIES ||============================== //
+
+const utilities = {
+  id: 'utilities',
+  title: 'Utilities',
+  type: 'group',
+  children: [
+    {
+      id: 'util-smart-npcs',
+      title: 'Smart NPCs',
+      type: 'item',
+      url: '/smart-npcs',
+      icon: icons.BarcodeOutlined
+    },
+    {
+      id: 'util-smart-nfts',
+      title: 'Create NFTs',
+      type: 'item',
+      url: '/create-nfts',
+      icon: icons.BarcodeOutlined
+    },
+    // {
+    //   id: 'util-typography',
+    //   title: 'Typography',
+    //   type: 'item',
+    //   url: '/typography',
+    //   icon: icons.FontSizeOutlined
+    // },
+    // {
+    //   id: 'util-color',
+    //   title: 'Color',
+    //   type: 'item',
+    //   url: '/color',
+    //   icon: icons.BgColorsOutlined
+    // },
+    // {
+    //   id: 'util-shadow',
+    //   title: 'Shadow',
+    //   type: 'item',
+    //   url: '/shadow',
+    //   icon: icons.BarcodeOutlined
+    // },
+  ]
+};
+
+export default utilities;
---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,63 @@
+import { lazy } from 'react';
+
+// project import
+import Loadable from 'components/Loadable';
+import Dashboard from 'layout/Dashboard';
+import { element } from 'prop-types';
+
+const Color = Loadable(lazy(() => import('pages/component-overview/color')));
+const Typography = Loadable(lazy(() => import('pages/component-overview/typography')));
+const Shadow = Loadable(lazy(() => import('pages/component-overview/shadows')));
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/index')));
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+const SmartNPCsPage = Loadable(lazy(() => import('pages/smart-npcs/sample-page')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <Dashboard />,
+  children: [
+    {
+      path: '/',
+      element: <DashboardDefault />
+    },
+    {
+      path: 'color',
+      element: <Color />
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    {
+      path: 'sample-page',
+      element: <SamplePage />
+    },
+    {
+      path: 'shadow',
+      element: <Shadow />
+    },
+    {
+      path: 'typography',
+      element: <Typography />
+    },
+    {
+      path: 'create-nfts',
+      element: <SamplePage />
+    },
+    {
+      path: 'smart-npcs',
+      element: <SmartNPCsPage />
+    }
+  ]
+};
+
+export default MainRoutes;
