81ac9604d505f0f1e16778709df16616701ad530
hohy <ghgud94@nexivil.com>
2024-08-27 23:42:17 +0900

    add smartnpc manage feature;

--- src/pages/smart-npcs/sample-page.jsx
+++ src/pages/smart-npcs/sample-page.jsx
@@ -1,6 +1,7 @@
 // material-ui
 import { Button, Card, MenuItem, Select, styled, TextField } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import Typography, { typographyClasses } from '@mui/material/Typography';
+import { nanoid } from 'nanoid';
 
 // project import
 // import MainCard from 'components/MainCard';
@@ -11,14 +12,49 @@ const StyledCard = styled(Card)(({ theme }) => ({
   flexDirection: 'column',
   marginBottom: theme.spacing(2),
   padding: theme.spacing(3),
-  [`& > .header`]: {
+  [`& > div`]: {
     display: 'flex',
+    flexDirection: 'column',
+    marginBottom: theme.spacing(2),
+    [`& > .${typographyClasses.root}`]: {
+      marginBottom: theme.spacing(1)
+    }
+  },
+  [`& > .header`]: {
     flexDirection: 'row',
     width: '100%',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  [`& > textarea`]: {
-    resize: 'vertical'
+  [`& > .history`]: {
+    // flexDirection: 'column',
+    [`& > textarea`]: {
+      resize: 'vertical'
+    }
+  },
+  [`& > .properties`]: {
+    // flexDirection: 'column'
+    [`& > .header`]: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      [`& > .dummy`]: {
+        flex: 1
+      },
+      // justifyContent: 'space-between',
+      marginBottom: theme.spacing(1)
+    },
+    [`& > .select-item`]: {
+      marginBottom: theme.spacing(1)
+    }
+  },
+  [`& > .actions`]: {
+    width: '100%',
+    justifyContent: 'flex-end',
+    marginBottom: 'unset'
+    // [`& > .${buttonBaseClasses.root}`]: {
+    //   width: 'fit-content',
+    // }
   }
 }));
 
@@ -31,11 +67,10 @@ const StyledActions = styled('div')(({ theme }) => ({
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const properties = ['smart', 'lazy', 'weak', 'goodLooking', 'goodSleeper'];
+const properties = ['normal', 'smart', 'lazy', 'weak', 'goodLooking', 'goodSleeper'];
 
-function CardItem({ info, onDelete }) {
+function CardItem({ info, onDelete, onAddProperties, onDeleteProperties }) {
   const [edit, setEdit] = useState(false);
-  // const [propsNum, setPropsNum] = useState(1);
 
   function handleEdit() {
     setEdit((prev) => !prev);
@@ -45,34 +80,58 @@ function CardItem({ info, onDelete }) {
     onDelete(info.id);
   }
 
+  function handleAdd() {
+    onAddProperties(info.name);
+  }
+
+  function handleDeleteProperties() {
+    onDeleteProperties(info.name);
+  }
+
   return (
     <StyledCard>
       <div className="header">
         {edit ? <TextField defaultValue={info.name} /> : <Typography>{info.name}</Typography>}
         <Button onClick={handleEdit}>{edit ? 'Save' : 'Edit'}</Button>
       </div>
-      <Typography>History</Typography>
-      <textarea defaultValue={info.history} disabled={!edit} />
-      <Typography>Properties</Typography>
-      <Select defaultValue={0}>
-        {properties.map((v, idx) => (
-          <MenuItem key={v} value={idx}>
-            {v}
-          </MenuItem>
+      <div className="history">
+        <Typography>History</Typography>
+        <textarea defaultValue={info.history} disabled={!edit} />
+      </div>
+      <div className="properties">
+        <div className="header">
+          <Typography>Properties</Typography>
+          <div className="dummy" />
+          <Button disabled={!edit} onClick={handleAdd}>
+            Add
+          </Button>
+          <Button disabled={!edit} onClick={handleDeleteProperties}>
+            Delete
+          </Button>
+        </div>
+        {info.properties.map((v, idx) => (
+          <Select key={idx} className="select-item" defaultValue={v} disabled={!edit}>
+            {properties.map((v, idx) => (
+              <MenuItem key={idx} value={v}>
+                {v}
+              </MenuItem>
+            ))}
+          </Select>
         ))}
-      </Select>
-      <Button onClick={handleDelete}>Delete</Button>
+      </div>
+      <div className="actions">
+        <Button onClick={handleDelete}>Delete</Button>
+      </div>
     </StyledCard>
   );
 }
 
 export default function SamplePage() {
-  console.log('atas');
   const [npcs, setNpcs] = useState([]);
 
   function handleCreate() {
     setNpcs((prev) => {
-      prev.push({ history: '', properties: [], name: `SmartNpc${prev.length + 1}` });
+      prev.push({ history: '', properties: ['normal'], name: `SmartNpc`, id: nanoid() });
       return [...prev];
     });
   }
@@ -80,22 +139,45 @@ export default function SamplePage() {
   function handleDelete(id) {
     setNpcs((prev) => {
       const _idx = prev.findIndex((v) => v.id === id);
-      prev.splice(0, _idx);
+      console.log(_idx);
+      prev.splice(_idx, 1);
+      return [...prev];
+    });
+  }
+
+  function handleAddProperties(name) {
+    setNpcs((prev) => {
+      prev.map((v) => v.name === name && v.properties.push('normal'));
+
+      return [...prev];
+    });
+  }
+
+  function handleDeleteProperties(name) {
+    setNpcs((prev) => {
+      prev.map((v) => v.name === name && v.properties.pop());
+
       return [...prev];
     });
   }
 
   useEffect(() => {
     setNpcs((prev) => {
-      prev.push({ history: 'test', properties: ['greed', 'smart'], name: 'SmartNpc1' });
+      prev.push({ history: 'test', properties: ['normal'], name: 'SmartNpc', id: nanoid() });
       return [...prev];
     });
   }, []);
-  console.log(npcs);
+
   return (
     <>
       {npcs.map((v, idx) => (
-        <CardItem info={v} key={idx} onDelete={handleDelete} />
+        <CardItem
+          info={v}
+          key={idx}
+          onDelete={handleDelete}
+          onAddProperties={handleAddProperties}
+          onDeleteProperties={handleDeleteProperties}
+        />
       ))}
       <StyledActions>
         <Button onClick={handleCreate}>Create</Button>
